447655f7a2255640baa0bf84ed4fea870ed69b84
Borse Tushar Mukesh <122999869+TusharB45@users.noreply.github.com>
2025-06-10 22:03:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,56 +1,57 @@
-const c=document.getElementById("gameCanvas")
-const ctx=c.getContext("2d")
+const c = document.getElementById("gameCanvas")
+const ctx = c.getContext("2d")
 
-let t=0
-const space=50
-const img=new Image()
-img.src="Rocket.png"
-
-function getY(time){
-  return Math.pow(1.22,time)*5
+function resize() {
+  c.width = window.innerWidth
+  c.height = window.innerHeight
 }
+window.addEventListener("resize", resize)
+resize()
 
-function draw()
+let t = 0
+const img = new Image()
+img.src = "Rocket.png"
 
-{
-ctx.clearRect(0,0,c.width,c.height)
+const getY = x => Math.pow(1.22, x) * 5
 
-ctx.beginPath()
-ctx.moveTo(0,c.height)
+function draw() {
+  ctx.clearRect(0, 0, c.width, c.height)
 
-for(let i=0;i<=t;i+=0.1){
-  let x=i*space
-  let y=c.height-getY(i)
-  ctx.lineTo(x,y)
-}
+  const sp = c.width / 25
+  const pts = []
 
-ctx.lineTo(t*space,c.height)
-ctx.closePath()
-ctx.fillStyle="rgba(204,0,51,0.5)"
-ctx.fill()
+  for (let i = 0; i <= t; i += 0.05) {
+    const x = i * sp
+    const y = c.height - getY(i)
+    pts.push([x, y])
+  }
 
-ctx.beginPath()
-ctx.moveTo(0,c.height)
+  ctx.beginPath()
+  ctx.moveTo(0, c.height)
+  pts.forEach(p => ctx.lineTo(p[0], p[1]))
+  ctx.lineTo(pts[pts.length - 1][0], c.height)
+  ctx.closePath()
+  ctx.fillStyle = "rgba(204,0,51,0.5)"
+  ctx.fill()
 
-for(let i=0;i<=t;i+=0.1){
-  let x=i*space
-  let y=c.height-getY(i)
-  ctx.lineTo(x,y)
-}
-ctx.strokeStyle="#cc0033"
-ctx.lineWidth=2
-ctx.stroke()
+  ctx.beginPath()
+  ctx.moveTo(0, c.height)
+  pts.forEach(p => ctx.lineTo(p[0], p[1]))
+  ctx.strokeStyle = "#cc0033"
+  ctx.lineWidth = 2
+  ctx.stroke()
 
-let rocketX=t*space
-let rocketY=c.height-getY(t)
-if(rocketX<c.width && rocketY>0){
-  ctx.drawImage(img,rocketX-20,rocketY-40,40,40)
-  t+=0.05
+  const x = t * sp
+  const y = c.height - getY(t)
+
+  if (x < c.width && y > -50) {
+    ctx.drawImage(img, x - 20, y - 40, 40, 40)
+    t += 0.08
+  } else {
+    t = 0
+  }
+
   requestAnimationFrame(draw)
 }
-else
-{
-  console.log("rocket gone")
-}
-}
-img.onload=function(){draw()}
+
+img.onload = () => draw()
